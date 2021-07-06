629386dd1973610a701de79a9841297f954886ca
miguelesco <miuelangel1999@gmail.com>
2021-07-06 15:33:54 -0400

    Winners generation

---
+++ modules/home/cardsGeneration.js
@@ -0,0 +1,65 @@
+
+function CardsGeneration() {
+  const projectSection = document.getElementById('winners');
+
+  const cardChild = [];
+  const content = [
+    {
+      name: "TYSON 'TENZ'",
+      team: 'Team Sentinel ',
+      description: "Is a Canadian player who currently plays for Sentinels. He is a retired professional Counter-Strike: Global Offensive player.",
+      imageURL: './assets/images/sen-tenz.jpg',
+    },
+    {
+      name: "TYSON 'TENZ'",
+      team: 'Team Sentinel ',
+      description: "Is a Canadian player who currently plays for Sentinels. He is a retired professional Counter-Strike: Global Offensive player.",
+      imageURL: './assets/images/sen-tenz.jpg',
+    },
+  ];
+
+  const createElement = (elementType = '', elementContent, elementClass = '', elementId = '') => {
+    const newElement = document.createElement(elementType);
+    newElement.innerHTML = elementContent;
+    if (elementClass) newElement.className = elementClass;
+    if (elementId) newElement.id = elementId;
+    return newElement;
+  };
+
+  const sectionTitle = createElement('h1', 'Featured Speakers');
+  const divider = createElement('div', '', 'divider');
+  const card = [];
+  const cardContainer = createElement('ul', '');
+
+  const renderCardContent = () => {
+    content.forEach((cardContent, i) => {
+      const {
+        name, team, description, imageURL
+      } = cardContent;
+
+      card.push(
+        `<li>
+          <img src="${imageURL}" alt="${name}">
+          <div>
+            <h2>${name}</h2>
+            <p>${team}</p>
+            <p class="description">${description}</p>
+          </div>
+        </li>`
+      );
+
+      let htmlChild = card[i];
+      cardContainer.innerHTML += htmlChild;
+    });
+  };
+
+  renderCardContent();
+
+  projectSection.append(
+    sectionTitle,
+    divider,
+    cardContainer,
+  );
+}
+
+export default CardsGeneration;
---
+++ modules/home/main.js
@@ -0,0 +1,8 @@
+import CardsGeneration from './cardsGeneration.js';
+
+function init () {
+  CardsGeneration();
+  console.log('sasa')
+} 
+
+init();
